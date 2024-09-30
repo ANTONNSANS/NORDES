042bf8a548a85dbcc4916fe0f8e2bd08c8ed8d54
ANTONNSANS <antonnsans@gmail.com>
2024-09-30 16:31:27 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Muestra un mensaje cuando se carga la página
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Página cargada completamente');
+});
+
+// Muestra un mensaje al hacer clic en un botón
+function mostrarMensaje() {
+    alert('¡Has hecho clic en el botón!');
+}
